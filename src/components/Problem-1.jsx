41e2f8f9f89d28e7c2f08717e470836065cff3ac
Taqinur <taqi1662@gmail.com>
2023-02-12 11:56:13 +0600

    first problem solved

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -5,8 +5,40 @@ const Problem1 = () => {
     const [show, setShow] = useState('all');
 
     const handleClick = (val) =>{
-        setShow(val);
+        if (val ==='all'){
+            setShow('all');
+            setTaskStatus(tasks.sort((a,b)=> {
+                let statusA = a.status;
+                let statusB = b.status;
+                
+                if (statusA < statusB){
+                    return -1;
+                }
+                if (statusA > statusB){
+                    return 1;
+                }
+            }));
+        }
+        else if (val === 'active'){
+            setShow('active');
+            setTaskStatus(tasks.filter(task => task.status.includes('active')));
+        }
+        else if (val === 'completed'){
+            setShow('completed');
+            setTaskStatus(tasks.filter(task => task.status.includes('completed')));
+        }
     }
+
+    const [tasks, setTasks] = useState([]);
+    console.log(tasks);
+
+    const handleTasks = (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const status = event.target.status.value;
+        setTasks([...tasks, {name, status}]);
+    }
+    const [taskStatus, setTaskStatus] = useState([]);
 
     return (
 
@@ -14,12 +46,13 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form className="row gy-2 gx-3 align-items-center mb-4" onSubmit={handleTasks}>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input
+                            name='name' type="text" className="form-control" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input name='status' type="text" className="form-control" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -47,7 +80,14 @@ const Problem1 = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        
+                            {
+                                taskStatus.map(
+                                    (task, i) => <tr key={i}>
+                                    <td scope='col'>{task.name}</td>
+                                    <td scope='col'>{task.status}</td>
+                                </tr>
+                                )
+                            }
                         </tbody>
                     </table>
                 </div>
